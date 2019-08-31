01780155f703f546df2c0f2b3b8a4c1772d4f092
s8nt2l <s8nt2l@gmail.com>
2019-08-31 14:01:15 +0300

    #3
    Add BulletManager, GunManager

--- Elephant/Assets/_Complete-Game/Scripts/Player/PlayerShooting.cs
+++ Elephant/Assets/_Complete-Game/Scripts/Player/PlayerShooting.cs
@@ -1,13 +1,11 @@
 ﻿using UnityEngine;
 using UnitySampleAssets.CrossPlatformInput;
 
-namespace CompleteProject
-{
-    public class PlayerShooting : MonoBehaviour
-    {
-        public int damagePerShot = 20;                  // The damage inflicted by each bullet.
-        public float timeBetweenBullets = 0.15f;        // The time between each shot.
-        public float range = 100f;                      // The distance the gun can fire.
+namespace CompleteProject {
+    public class PlayerShooting : MonoBehaviour {
+        private int damagePerShot;                  // The damage inflicted by each bullet.
+        private float timeBetweenBullets;        // The time between each shot.
+        private float range;                      // The distance the gun can fire.
 
 
         float timer;                                    // A timer to determine when to fire.
@@ -18,35 +16,60 @@ namespace CompleteProject
         LineRenderer gunLine;                           // Reference to the line renderer.
         AudioSource gunAudio;                           // Reference to the audio source.
         Light gunLight;                                 // Reference to the light component.
-		public Light faceLight;								// Duh
-        float effectsDisplayTime = 0.2f;                // The proportion of the timeBetweenBullets that the effects will display for.
+        public Light faceLight;								// Duh
+        float effectsDisplayTime;                // The proportion of the timeBetweenBullets that the effects will display for.
 
 
-        void Awake ()
-        {
+        void UpdateValues() {
+            damagePerShot = Gun.damagePerShot;
+            timeBetweenBullets = Gun.timeBetweenBullets;
+            range = Gun.range;
+            effectsDisplayTime = Gun.effectsDisplayTime;
+        }
+
+        void Awake() {
             // Create a layer mask for the Shootable layer.
-            shootableMask = LayerMask.GetMask ("Shootable");
+            shootableMask = LayerMask.GetMask("Shootable");
 
             // Set up the references.
-            gunParticles = GetComponent<ParticleSystem> ();
-            gunLine = GetComponent <LineRenderer> ();
-            gunAudio = GetComponent<AudioSource> ();
-            gunLight = GetComponent<Light> ();
-			//faceLight = GetComponentInChildren<Light> ();
+            gunParticles = GetComponent<ParticleSystem>();
+            gunLine = GetComponent<LineRenderer>();
+            gunAudio = GetComponent<AudioSource>();
+            gunLight = GetComponent<Light>();
+            //faceLight = GetComponentInChildren<Light> ();
+
         }
 
 
-        void Update ()
-        {
+        void Update() {
             // Add the time since Update was last called to the timer.
             timer += Time.deltaTime;
 
 #if !MOBILE_INPUT
             // If the Fire1 button is being press and it's time to fire...
-			if(Input.GetButton ("Fire1") && timer >= timeBetweenBullets && Time.timeScale != 0)
-            {
+            if (Input.GetButton("Fire1") && timer >= timeBetweenBullets && Time.timeScale != 0) {
                 // ... shoot the gun.
-                Shoot ();
+                if (!BulletManager.Empty()) {
+                    BulletManager.Shoot();
+                    //GunManager.current_gun.Shoot();
+                    Shoot();
+                }
+            }
+
+            if (Input.GetButton("Fire2")) {
+                BulletManager.Reload();
+            }
+            if (Input.GetKey(KeyCode.Alpha1)) {
+                GunManager.SetType(GunType.Ak47);
+                UpdateValues();
+            }
+            if (Input.GetKey(KeyCode.Alpha2)) {
+                GunManager.SetType(GunType.Laser);
+                UpdateValues();
+            }
+            if (Input.GetKey(KeyCode.Alpha3)) {
+                GunManager.SetType(GunType.SimpleGun);
+                UpdateValues();
             }
 #else
             // If there is input on the shoot direction stick and it's time to fire...
@@ -57,68 +80,58 @@ namespace CompleteProject
             }
 #endif
             // If the timer has exceeded the proportion of timeBetweenBullets that the effects should be displayed for...
-            if(timer >= timeBetweenBullets * effectsDisplayTime)
-            {
+            if (timer >= timeBetweenBullets * effectsDisplayTime) {
                 // ... disable the effects.
-                DisableEffects ();
+                DisableEffects();
             }
         }
 
 
-        public void DisableEffects ()
-        {
+        public void DisableEffects() {
             // Disable the line renderer and the light.
             gunLine.enabled = false;
-			faceLight.enabled = false;
+            faceLight.enabled = false;
             gunLight.enabled = false;
         }
 
 
-        void Shoot ()
-        {
+        void Shoot() {
             // Reset the timer.
             timer = 0f;
 
             // Play the gun shot audioclip.
-            gunAudio.Play ();
+            gunAudio.Play();
 
             // Enable the lights.
             gunLight.enabled = true;
-			faceLight.enabled = true;
+
 
             // Stop the particles from playing if they were, then start the particles.
-            gunParticles.Stop ();
-            gunParticles.Play ();
+            gunParticles.Stop();
+            gunParticles.Play();
 
-            // Enable the line renderer and set it's first position to be the end of the gun.
+            faceLight.enabled = true;
             gunLine.enabled = true;
+            //GunManager.current_gun.Shoot(ref gunLine, ref shootRay, ref shootHit, shootableMask, faceLight);
             gunLine.SetPosition (0, transform.position);
 
-            // Set the shootRay so that it starts at the end of the gun and points forward from the barrel.
+
             shootRay.origin = transform.position;
             shootRay.direction = transform.forward;
 
-            // Perform the raycast against gameobjects on the shootable layer and if it hits something...
-            if(Physics.Raycast (shootRay, out shootHit, range, shootableMask))
-            {
-                // Try and find an EnemyHealth script on the gameobject hit.
-                EnemyHealth enemyHealth = shootHit.collider.GetComponent <EnemyHealth> ();
 
-                // If the EnemyHealth component exist...
-                if(enemyHealth != null)
-                {
-                    // ... the enemy should take damage.
-                    enemyHealth.TakeDamage (damagePerShot, shootHit.point);
+            if (Physics.Raycast(shootRay, out shootHit, range, shootableMask)) {
+
+                EnemyHealth enemyHealth = shootHit.collider.GetComponent<EnemyHealth>();
+
+
+                if (enemyHealth != null) {
+                    enemyHealth.TakeDamage(damagePerShot, shootHit.point);
                 }
 
-                // Set the second position of the line renderer to the point the raycast hit.
-                gunLine.SetPosition (1, shootHit.point);
-            }
-            // If the raycast didn't hit anything on the shootable layer...
-            else
-            {
-                // ... set the second position of the line renderer to the fullest extent of the gun's range.
-                gunLine.SetPosition (1, shootRay.origin + shootRay.direction * range);
+
+
+                gunLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
             }
         }
     }
